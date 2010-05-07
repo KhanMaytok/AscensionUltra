96bccee40f6d38e9617e7090a4849cb6197fa0ed
Face <Face@localhost>
2010-05-07 21:58:01 +0200

    Added MFD data infrastructure.

--- Orbitersdk/samples/AscensionUltra/AscensionTower.cpp
+++ Orbitersdk/samples/AscensionUltra/AscensionTower.cpp
@@ -24,19 +24,18 @@ int g_MFDmode; // identifier for new MFD mode
 
 DLLCLBK void opcDLLInit (HINSTANCE hDLL)
 {
-	static char *name = "Ascension Tower";   // MFD mode name
+	static char *name = "Ascension Tower";
 	MFDMODESPEC spec;
 	spec.name = name;
-	spec.key = OAPI_KEY_T;                // MFD mode selection key
-	spec.msgproc = AscensionTower::MsgProc;  // MFD mode callback function
-
-	// Register the new MFD mode with Orbiter
+	spec.key = OAPI_KEY_T;
+	spec.msgproc = AscensionTower::MsgProc;
 	g_MFDmode = oapiRegisterMFDMode (spec);
+	g_Bar=NULL;
 }
 
 DLLCLBK void opcDLLExit (HINSTANCE hDLL)
 {
-	// Unregister the custom MFD mode when the module is unloaded
+	if (g_Bar!=NULL) DeleteObject(g_Bar);
 	oapiUnregisterMFDMode (g_MFDmode);
 }
 
@@ -44,10 +43,26 @@ DLLCLBK void opcDLLExit (HINSTANCE hDLL)
 // MFD class implementation
 
 // Constructor
-AscensionTower::AscensionTower (DWORD w, DWORD h, VESSEL *vessel)
+AscensionTower::AscensionTower (UINT mfd, DWORD w, DWORD h, VESSEL *vessel)
 : MFD (w, h, vessel)
-{
-	// Add MFD initialisation here
+{	
+	width=(int)w/35;
+	height=(int)h/28;
+
+	OBJHANDLE obj=vessel->GetHandle();
+	std::map<UINT, MFDData *> *mfds=g_MFDData[obj];
+	if (mfds==NULL)
+	{
+		mfds=new std::map<UINT, MFDData *>;
+		g_MFDData[obj]=mfds;
+	}
+	MFDData *mfdData=(*mfds)[mfd];
+	if (mfdData==NULL)
+	{
+		mfdData=new MFDData();
+		(*mfds)[mfd]=mfdData;
+	}
+	data=mfdData;
 }
 
 // Destructor
@@ -69,8 +84,8 @@ int AscensionTower::ButtonMenu (const MFDBUTTONMENU **menu) const
 {
 	// The menu descriptions for the two buttons
 	static const MFDBUTTONMENU mnu[2] = {
-		{"Move up", 0, '['},
-		{"Move down", 0, ']'}
+		{"Move up", 0, 'U'},
+		{"Move down", 0, 'D'}
 	};
 	if (menu) *menu = mnu;
 	return 2; // return the number of buttons used
@@ -80,11 +95,25 @@ int AscensionTower::ButtonMenu (const MFDBUTTONMENU **menu) const
 // Repaint the MFD
 void AscensionTower::Update (HDC hDC)
 {
-	Title (hDC, "Ascension Tower");
-	// Draws the MFD title
+	//Creating the pen for drawing the progress bar
+	if (g_Bar==NULL)
+	{
+		LOGPEN pen;
+		SelectDefaultPen(hDC, 2);
+		GetObject(GetCurrentObject(hDC, OBJ_PEN), sizeof(LOGPEN), &pen);
+		g_MiddleGreen=pen.lopnColor;
+		g_Bar=CreateSolidBrush(g_MiddleGreen);
+	}
 
-	// Add MFD display routines here.
-	// Use the device context (hDC) for Windows GDI paint functions.
+	Title (hDC, "Ascension Tower");
+	char line[20];
+	int l=0;
+
+	//Descriptions (normal, light green)
+	SelectDefaultFont (hDC, 0);
+	sprintf(line, "%d", data->page);
+	l=strlen(line);
+	TextOut(hDC, (1+(37-l)/2)*width, (5*height) >> 1, line, l);
 }
 
 // MFD message parser
@@ -94,8 +123,43 @@ int AscensionTower::MsgProc (UINT msg, UINT mfd, WPARAM wparam, LPARAM lparam)
 	case OAPI_MSG_MFD_OPENED:
 		// Our new MFD mode has been selected, so we create the MFD and
 		// return a pointer to it.
-		return (int)(new AscensionTower (LOWORD(wparam), HIWORD(wparam), (VESSEL*)lparam));
+		return (int)(new AscensionTower (mfd, LOWORD(wparam), HIWORD(wparam), (VESSEL*)lparam));
 	}
 	return 0;
 }
 
+// Handling shortcut keys
+bool AscensionTower::ConsumeKeyBuffered(DWORD key)
+{	
+	bool result=true;
+	switch(key)
+	{
+	case OAPI_KEY_U://Up
+		data->page++;
+		break;
+	case OAPI_KEY_D://Down
+		data->page--;
+		break;
+	default:
+		result=false;
+	}
+	if (!result) return false;
+	InvalidateDisplay();
+	return true;
+}
+
+// Handling button presses by forwarding shortcuts
+bool AscensionTower::ConsumeButton(int bt, int event)
+{	
+	if (event & PANEL_MOUSE_LBDOWN)
+	{
+		switch(bt)
+		{
+		case 0:
+			return ConsumeKeyBuffered(OAPI_KEY_U);
+		case 1:
+			return ConsumeKeyBuffered(OAPI_KEY_D);
+		}
+	}	
+	return false;
+}
--- Orbitersdk/samples/AscensionUltra/AscensionTower.h
+++ Orbitersdk/samples/AscensionUltra/AscensionTower.h
@@ -11,14 +11,30 @@
 #ifndef __ASCENSIONTOWER_H
 #define __ASCENSIONTOWER_H
 
+#include <map>
+
+struct MFDData
+{
+	int page;
+};
+
 class AscensionTower: public MFD {
 public:
-	AscensionTower (DWORD w, DWORD h, VESSEL *vessel);
+	AscensionTower (UINT mfd, DWORD w, DWORD h, VESSEL *vessel);
 	~AscensionTower ();
 	char *ButtonLabel (int bt);
 	int ButtonMenu (const MFDBUTTONMENU **menu) const;
 	void Update (HDC hDC);
+	bool AscensionTower::ConsumeKeyBuffered(DWORD key);
+	bool AscensionTower::ConsumeButton(int bt, int event);
 	static int MsgProc (UINT msg, UINT mfd, WPARAM wparam, LPARAM lparam);
+private:
+	MFDData *data;
+	int width, height;
 };
+
+std::map<OBJHANDLE, std::map<UINT, MFDData *> *> g_MFDData;
+HBRUSH g_Bar;
+COLORREF g_MiddleGreen;
 
 #endif // !__ASCENSIONTOWER_H
