100e8edc9f2aea96099d7389a693d4a4389b37ca
Face <Face@localhost>
2012-06-25 19:06:40 +0200

    AscensionTower: added Page class marking begin of refactoring to subclassing

---
+++ Orbitersdk/samples/AscensionUltra/AscensionTowerPage.h
@@ -0,0 +1,81 @@
+#pragma once
+#include <vector>
+#include "AscensionUltra.h"
+#include "orbitersdk.h"
+
+#define WRITEMFD_HALFLINES		0x01
+#define WRITEMFD_RIGHTALINED	0x04
+#define WRITEMFD_HIGHLIGHTED	0x08
+
+#define HIGHLIGHTED(flags)	(flags & WRITEMFD_HIGHLIGHTED)>0
+#define HALFLINES(flags)	(flags & WRITEMFD_HALFLINES)>0
+#define RIGHTALINED(flags)	(flags & WRITEMFD_RIGHTALINED)>0
+
+typedef enum AscensionTowerPageInstance
+{
+	BaseSelect,
+	MainMenu,
+	GroundMenu,
+	ATCMenu,
+	HangarForRoomSelection,
+	RoomSelection,
+	HangarForDoorSelection,
+	DoorSelection,
+	TaxiRouteStartSelection,
+	TaxiRouteEndSelection,
+	HangarForCraneSelection,
+	PassengerTerminal,
+	Fueling,
+	LaunchPrepare,
+	Bearing,
+	LandingRunwaySelection,
+	Launch,
+	DoorControl,
+	CraneControl,
+	Roster,
+	PersonControl,
+	RoomForPersonSelection,
+	HangarForPersonSelection,
+	PassengerTransfer,
+	CraneList,
+	CraneGrapple,
+	Reset,
+};
+
+struct AscensionTowerListPair
+{
+	UINT Index;
+	char *Name;
+};
+
+extern HBRUSH g_Bar;
+extern COLORREF g_MiddleGreen;
+static int AT_BUTTON[6], AT_BUTTONDOUBLED[10];
+
+class AscensionTowerPage
+{
+public:
+	AscensionTowerPage(void);
+	~AscensionTowerPage(void);
+
+	virtual void RenderPage();
+	virtual int GetListSize();
+	virtual AscensionTowerListPair GetListItem(int index);
+	virtual void Select(int index=-1);
+	virtual char *GetButtonLabel (int bt);
+	virtual int GetButtonMenu (MFDBUTTONMENU *mnu);
+	virtual bool SetButton(int bt);
+	virtual bool SetKey(DWORD key);
+	virtual char *GetTitle();
+	virtual char *GetSubtitle();
+	
+protected:
+	AscensionUltra *ascension;
+	MFD *mfd;
+	float width, height;
+	int mfdWidth, mfdHeight;
+	HDC hDC;
+	int selectedIndex, selection, page;
+	char *GetNameSafeTitle(char *title, char *trailer);
+	void WriteMFD(char *text, int line=-1, int column=-1, int flags=0);
+};
