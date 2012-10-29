0338b5b4d07ef4a6879af8be85b4384202d7955f
Face <Face@localhost>
2012-10-29 18:10:12 +0100

    Pages: combined code and header files
    There is no need to keep the various page classes split into code and header
    files. For sake of better overview, all code is merged into the header being
    included in AscensionTowerData.

--- Orbitersdk/samples/AscensionUltra/BasePage.h
+++ Orbitersdk/samples/AscensionUltra/BasePage.h
@@ -1,4 +1,5 @@
 #pragma once
+#pragma warning(disable : 4482)
 #include "AscensionTowerPage.h"
 
 class BasePage: public AscensionTowerPage
@@ -23,3 +24,135 @@ private:
 	void Scan();
 	void SetAscension(int index);
 };
+
+BasePage::BasePage(AscensionTowerData *data):AscensionTowerPage(data)
+{
+	ascensionHandle=NULL;
+	ascensionName=NULL;
+}
+
+BasePage::~BasePage(void)
+{
+	delete [] ascensionName;
+	for (std::vector<AscensionTowerListPair>::iterator i=scanList.begin(); i!=scanList.end(); i++) delete [] i->Name;
+	scanList.clear();
+}
+
+// Resolves object reference by means of:
+// 1. using the index, check pointer
+// 2. get all vessels of class AscensionUltra, check for unique name
+// 3. return NULL
+AscensionUltra *BasePage::GetAscension()
+{	
+	if (oapiIsVessel(ascensionHandle)) return ascension;
+	Scan();
+	int detected=-1;
+	if (ascensionName!=NULL) for (std::vector<AscensionTowerListPair>::iterator i=scanList.begin(); i!=scanList.end(); i++)
+	{
+		if (strcmp(i->Name, ascensionName)==0)
+		{
+			if (detected>=0)
+			{
+				return ascension=NULL; //Name is not unique
+			}
+			detected=i->Index;			
+		}
+	}
+	if (detected<0)
+	{
+		if (scanList.size()!=1)
+		{
+			return ascension=NULL;
+		}
+		detected=scanList.begin()->Index;
+	}
+	SetAscension(detected);
+	return ascension;
+}
+
+void BasePage::SetAscension(int index)
+{
+	ascension=NULL;
+	if (index<0 || index>=(int)oapiGetVesselCount()) return Scan();
+	ascensionHandle=oapiGetVesselByIndex(index);
+	VESSEL *vessel=oapiGetVesselInterface(ascensionHandle);	
+	if (strcmp(vessel->GetClassName(), "AscensionUltra")!=0) return Scan();
+	ascension=(AscensionUltra *)vessel;
+	char *name=ascension->GetName();
+	double version=ascension->GetVersion();
+	if (version!=1.0)
+	{
+		ascension=NULL;
+		sprintf(oapiDebugString(), "%s has version %f (need 1.0). Please upgrade MFD and/or vessel.", name, version);
+		return Scan();
+	}
+	delete [] ascensionName;
+	strcpy(ascensionName=new char[strlen(name)+1], name);	
+}
+
+void BasePage::Scan()
+{
+	for (std::vector<AscensionTowerListPair>::iterator i=scanList.begin(); i!=scanList.end(); i++) delete [] i->Name;
+	scanList.clear();
+	int k=oapiGetVesselCount();
+	char line[20]; //enough for int conversion base 10
+	char *target;
+	for (int i=0;i<k;i++)
+	{
+		VESSEL *vessel=oapiGetVesselInterface(oapiGetVesselByIndex(i));	
+		if (strcmp(vessel->GetClassName(), "AscensionUltra")==0)
+		{
+			char *source=vessel->GetName();
+			int l=strlen(_itoa(i, line, 10));
+			int sum=strlen(source)+l+3;
+			if (sum<37) sprintf(target=new char[sum+1], "[%s] %s", line, source);
+			else
+			{
+				sprintf(target=new char[37], "[%s] ", line);
+				strncat(target, source, 31-l);
+				strcat(target, "..");
+			}
+			AscensionTowerListPair pair={i,target};
+			scanList.push_back(pair);
+		}
+	}	
+}
+
+AscensionTowerPageInstance BasePage::Select(int index)
+{
+	AscensionTowerPage::Select(index);
+	SetAscension(selectedIndex);
+	return MainMenu;
+}
+
+// Return button labels
+char *BasePage::LabelRenderer (int bt)
+{
+	switch (bt)
+	{
+		case 6: return "BAS";
+		default: return AscensionTowerPage::LabelRenderer(bt);
+	}
+}
+
+
+// Return button menus
+int BasePage::MenuRenderer (MFDBUTTONMENU *mnu)
+{	
+	static MFDBUTTONMENU menu={"Scan for bases",NULL,'H'};
+	mnu[0].line1="Select base";
+	mnu[6]=menu;
+	return AscensionTowerPage::MenuRenderer(mnu);
+}
+
+// Handling shortcut keys
+AscensionTowerPageInstance BasePage::KeyHandler(DWORD key)
+{	
+	switch(key)
+	{	
+	case OAPI_KEY_H://Scan for bases
+		return BaseSelect;
+	default:
+		return AscensionTowerPage::KeyHandler(key);
+	}
+}
--- Orbitersdk/samples/AscensionUltra/MainPage.h
+++ Orbitersdk/samples/AscensionUltra/MainPage.h
@@ -1,4 +1,5 @@
 #pragma once
+#pragma warning(disable : 4482)
 #include "AscensionTowerPage.h"
 
 class MainPage: public AscensionTowerPage
@@ -20,3 +21,49 @@ protected:
 		{4, " Reset to Defaults"}};return menu[index];}
 	AscensionTowerPageInstance Select(int index=-1);
 };
+
+AscensionTowerPageInstance MainPage::Select(int index)
+{
+	AscensionTowerPage::Select(index);
+	switch(selection)
+	{
+		case 0: return GroundMenu;
+		case 1: return ATCMenu;
+		case 2: return HangarForRoomSelection;
+		case 3: return Roster;
+		case 4: return Reset;
+		default: return Undefined;
+	}
+}
+
+// Return button labels
+char *MainPage::LabelRenderer (int bt)
+{
+	switch (bt)
+	{
+		case 6: return "BAS";
+		default: return AscensionTowerPage::LabelRenderer(bt);
+	}
+}
+
+
+// Return button menus
+int MainPage::MenuRenderer (MFDBUTTONMENU *mnu)
+{	
+	static MFDBUTTONMENU menu = {"Scan for bases", NULL, 'H'};
+	mnu[0].line1="Select request";
+	mnu[6]=menu;
+	return AscensionTowerPage::MenuRenderer(mnu);
+}
+
+// Handling shortcut keys
+AscensionTowerPageInstance MainPage::KeyHandler(DWORD key)
+{	
+	switch(key)
+	{	
+	case OAPI_KEY_H://Scan for bases
+		return BaseSelect;
+	default:
+		return AscensionTowerPage::KeyHandler(key);
+	}
+}
