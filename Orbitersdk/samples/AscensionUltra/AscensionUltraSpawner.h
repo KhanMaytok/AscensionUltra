f386f1f10f8dad9a4a420558327103b2462ea4bf
Face <Face@localhost>
2013-07-28 00:59:39 +0200

    AscensionUltraSpawner: loading and setting up ATC display slots

--- Orbitersdk/samples/AscensionUltra/AscensionUltraSpawner.h
+++ Orbitersdk/samples/AscensionUltra/AscensionUltraSpawner.h
@@ -7,6 +7,7 @@ public:
 	AscensionUltraSpawner(HINSTANCE hDLL);
 	~AscensionUltraSpawner();
 	void clbkSimulationStart (RenderMode mode);
+	void clbkSimulationEnd();
 	void clbkFocusChanged(OBJHANDLE new_focus, OBJHANDLE old_focus);
 private:
 	void Save();
