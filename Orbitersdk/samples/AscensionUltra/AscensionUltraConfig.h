6a1d4bc0ec9b9f33bb73d0c450126bc01a9bc4da
Face <Face@localhost>
2014-07-05 20:59:07 +0200

    Documentation: unified header usage for GPL and copyright notice

--- Orbitersdk/samples/AscensionUltra/AscensionUltraConfig.h
+++ Orbitersdk/samples/AscensionUltra/AscensionUltraConfig.h
@@ -1,13 +1,20 @@
-// ==============================================================
-//        ORBITER STARTUP MODULE: ASCENSIONULTRACONFIGURATOR
-//                  Part of Ascension Ultra
-//          Copyright (C) 2010 Friedrich Kastner-Masilko
-//                   All rights reserved
+// Copyright (C) 2010-2014 Friedrich Kastner-Masilko
 //
-// AscensionUltraConfigurator.cpp
-// Implementation of Ascension Ultra startup module.
-// Contains launchpad items and autoloading tools.
-// ==============================================================
+// This file is part of the Ascension Ultra Project
+//
+// Ascension Ultra is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 2 of
+// the License, or (at your option) any later version.
+//
+// Ascension Ultra is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Ascension Ultra. If not, see
+// <http://www.gnu.org/licenses/>.
 
 #pragma once
 #include "orbitersdk.h"
