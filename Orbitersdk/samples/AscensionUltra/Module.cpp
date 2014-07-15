aaa70b3fd7520613407b1bd2eebff7705c0c4c37
Face <Face@localhost>
2014-07-15 19:29:44 +0200

    Module: minor code cleanup

--- Orbitersdk/samples/AscensionUltra/Module.cpp
+++ Orbitersdk/samples/AscensionUltra/Module.cpp
@@ -33,15 +33,13 @@
 // Global parameters
 // ==============================================================
 
-struct GDIParams {
+struct GDIParams
+{
 	HINSTANCE hDLL;
-	/*HFONT font[2];
-	DWORD col[4];
-	HBRUSH brush[4];
-	HPEN pen[2];*/
 } g_Param;
 
-static HELPCONTEXT g_hc = {
+static HELPCONTEXT g_hc =
+{
 	"html/vessels/AscensionUltra.chm",
 	0,
 	"html/vessels/AscensionUltra.chm::/AscensionUltra.hhc",
@@ -59,21 +57,7 @@ void UpdateCtrlDialog (AscensionUltra *au, HWND hWnd = 0);
 DLLCLBK void InitModule (HINSTANCE hModule)
 {
 	srand((unsigned)time(0)); //Seed the random generator with a timestamp
-
 	g_Param.hDLL = hModule;
-	
-//	oapiRegisterCustomControls (hModule);
-
-	// allocate GDI resources
-	/*g_Param.font[0]  = CreateFont (-13, 0, 0, 0, 700, 0, 0, 0, 0, 0, 0, 0, 0, "Arial");
-	g_Param.font[1]  = CreateFont (-10, 0, 0, 0, 400, 0, 0, 0, 0, 0, 0, 0, 0, "Arial");
-	g_Param.brush[0] = CreateSolidBrush (RGB(0,255,0));    // green
-	g_Param.brush[1] = CreateSolidBrush (RGB(255,0,0));    // red
-	g_Param.brush[2] = CreateSolidBrush (RGB(80,80,224));  // blue
-	g_Param.brush[3] = CreateSolidBrush (RGB(160,120,64)); // brown
-	g_Param.pen[0] = CreatePen (PS_SOLID, 1, RGB(224,224,224));
-	g_Param.pen[1] = CreatePen (PS_SOLID, 3, RGB(164,164,164));*/
-		
 }
 
 void RotateMesh(MESHHANDLE mesh, float angle, VECTOR3 v, VECTOR3 ref)
@@ -401,41 +385,13 @@ void ReadATCParameters(BaseVessel::Talker::Voice &voice, const char *ini, const 
 	}
 }
 
-// --------------------------------------------------------------
-// Module cleanup
-// --------------------------------------------------------------
-DLLCLBK void ExitModule (HINSTANCE hModule)
-{
-//	oapiUnregisterCustomControls (hModule);
+DLLCLBK void ExitModule (HINSTANCE hModule){}
 
-	/*int i;
-	// deallocate GDI resources
-	for (i = 0; i < 2; i++) DeleteObject (g_Param.font[i]);
-	for (i = 0; i < 4; i++) DeleteObject (g_Param.brush[i]);
-	for (i = 0; i < 2; i++) DeleteObject (g_Param.pen[i]);*/
-}
+DLLCLBK VESSEL *ovcInit (OBJHANDLE hvessel, int flightmodel){return new AscensionUltra (hvessel, flightmodel);}
 
-// --------------------------------------------------------------
-// Vessel initialisation
-// --------------------------------------------------------------
-DLLCLBK VESSEL *ovcInit (OBJHANDLE hvessel, int flightmodel)
-{
-	// need to init device-dependent resources here in case the screen mode has changed
-	/*g_Param.col[2] = oapiGetColour(80,80,224);
-	g_Param.col[3] = oapiGetColour(160,120,64);*/
+DLLCLBK void ovcExit (VESSEL *vessel){if (vessel) delete (AscensionUltra*)vessel;}
 
-	return new AscensionUltra (hvessel, flightmodel);
-}
-
-// --------------------------------------------------------------
-// Vessel cleanup
-// --------------------------------------------------------------
-DLLCLBK void ovcExit (VESSEL *vessel)
-{
-	if (vessel)	delete (AscensionUltra*)vessel;
-}
-
-AscensionUltra *GetDG (HWND hDlg)
+AscensionUltra *GetBase (HWND hDlg)
 {
 	// retrieve AscensionUltra interface from scenario editor
 	OBJHANDLE hVessel;
@@ -454,10 +410,10 @@ BOOL CALLBACK EdPg1Proc (HWND hTab, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			oapiOpenHelp (&g_hc);
 			return TRUE;
 		case IDC_OLOCK_CLOSE:
-			GetDG(hTab)->GetHangar(HANGARTYPETA, 0)->GetDoor(0)->Close();
+			GetBase(hTab)->GetHangar(HANGARTYPETA, 0)->GetDoor(0)->Close();
 			return TRUE;
 		case IDC_OLOCK_OPEN:
-			GetDG(hTab)->GetHangar(HANGARTYPETA, 0)->GetDoor(0)->Open();
+			GetBase(hTab)->GetHangar(HANGARTYPETA, 0)->GetDoor(0)->Open();
 			return TRUE;
 		}
 		break;
@@ -480,11 +436,11 @@ BOOL CALLBACK EdPg2Proc (HWND hTab, UINT uMsg, WPARAM wParam, LPARAM lParam)
 // Message procedure for editor page 3 (damage)
 BOOL CALLBACK EdPg3Proc (HWND hTab, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	AscensionUltra *dg;
+	AscensionUltra *au;
 
 	switch (uMsg) {
 	case WM_INITDIALOG: {
-		dg = (AscensionUltra*)oapiGetVesselInterface ((OBJHANDLE)lParam);		
+		au = (AscensionUltra*)oapiGetVesselInterface ((OBJHANDLE)lParam);		
 		} break;
 	case WM_COMMAND:
 		break;
@@ -498,7 +454,7 @@ BOOL CALLBACK EdPg3Proc (HWND hTab, UINT uMsg, WPARAM wParam, LPARAM lParam)
 // Add vessel-specific pages into scenario editor
 DLLCLBK void secInit (HWND hEditor, OBJHANDLE hVessel)
 {
-	AscensionUltra *dg = (AscensionUltra*)oapiGetVesselInterface (hVessel);
+	AscensionUltra *au = (AscensionUltra*)oapiGetVesselInterface (hVessel);
 
 	EditorPageSpec eps1 = {"Animations", g_Param.hDLL, IDD_EDITOR_PG1, EdPg1Proc};
 	SendMessage (hEditor, WM_SCNEDITOR, SE_ADDPAGEBUTTON, (LPARAM)&eps1);
@@ -510,12 +466,12 @@ DLLCLBK void secInit (HWND hEditor, OBJHANDLE hVessel)
 // Message callback function for control dialog box
 BOOL CALLBACK Ctrl_DlgProc (HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	AscensionUltra *dg = (uMsg == WM_INITDIALOG ? (AscensionUltra*)lParam : (AscensionUltra*)oapiGetDialogContext (hWnd));
+	AscensionUltra *au = (uMsg == WM_INITDIALOG ? (AscensionUltra*)lParam : (AscensionUltra*)oapiGetDialogContext (hWnd));
 	// pointer to vessel instance was passed as dialog context
 
 	switch (uMsg) {
 	case WM_INITDIALOG:
-		UpdateCtrlDialog (dg, hWnd);
+		UpdateCtrlDialog (au, hWnd);
 		return FALSE;
 	case WM_COMMAND:
 		switch (LOWORD(wParam)) {
@@ -523,10 +479,10 @@ BOOL CALLBACK Ctrl_DlgProc (HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			oapiCloseDialog (hWnd);
 			return TRUE;
 		case IDC_OLOCK_CLOSE:
-			dg->GetHangar(HANGARTYPETA, 0)->GetDoor(0)->Close();
+			au->GetHangar(HANGARTYPETA, 0)->GetDoor(0)->Close();
 			return 0;
 		case IDC_OLOCK_OPEN:
-			dg->GetHangar(HANGARTYPETA, 0)->GetDoor(0)->Open();
+			au->GetHangar(HANGARTYPETA, 0)->GetDoor(0)->Open();
 			return 0;
 		}
 		break;
@@ -534,7 +490,7 @@ BOOL CALLBACK Ctrl_DlgProc (HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	return oapiDefDialogProc (hWnd, uMsg, wParam, lParam);
 }
 
-void UpdateCtrlDialog (AscensionUltra *dg, HWND hWnd)
+void UpdateCtrlDialog (AscensionUltra *au, HWND hWnd)
 {
 	static int bstatus[2] = {BST_UNCHECKED, BST_CHECKED};
 
@@ -543,7 +499,7 @@ void UpdateCtrlDialog (AscensionUltra *dg, HWND hWnd)
 
 	int op;
 
-	op = dg->GetHangar(HANGARTYPETA, 0)->GetDoor(0)->GetPosition()==0.0?0:1;
+	op = au->GetHangar(HANGARTYPETA, 0)->GetDoor(0)->GetPosition()==0.0?0:1;
 	SendDlgItemMessage (hWnd, IDC_OLOCK_OPEN, BM_SETCHECK, bstatus[op], 0);
 	SendDlgItemMessage (hWnd, IDC_OLOCK_CLOSE, BM_SETCHECK, bstatus[1-op], 0);	
 }
